f2d954a93d73fdb3baa9f6bf3902df32d4a9742c
LaureDaigneault <laurel2195@gmail.com>
2024-03-23 02:40:00 -0500

    fix: export missing style utilities

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,5 +1,5 @@
 import { InputThemeType, ThemeType, ShapeType } from './types';
-import { ThemeProvider, styled } from 'styled-components';
+import { ThemeProvider, createGlobalStyle, styled, useTheme } from 'styled-components';
 import { convertHexToRGB, getContrastText } from './contrast';
 import defaultInputTheme from './defaultInputTheme';
 import Handlebars from 'handlebars/dist/cjs/handlebars';
@@ -190,4 +190,4 @@ const createTheme = (inputTheme: InputThemeType): ThemeType => {
 // Compiling default input theme into a usable default theme
 const defaultTheme = createTheme(defaultInputTheme);
 
-export { defaultTheme, createTheme, getContrastText, ThemeProvider, styled };
+export { defaultTheme, createTheme, getContrastText, ThemeProvider, styled, useTheme, createGlobalStyle };
