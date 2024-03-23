2d218c9c20863d49b96cd8a963e22c522749a632
LaureDaigneault <laurel2195@gmail.com>
2024-03-23 12:27:44 -0500

    fix: eslint

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,5 +3,6 @@ import 'styled-components';
 import { ThemeType } from 'styles/theme';
 
 declare module 'styled-components' {
+  // eslint-disable-next-line
   export interface DefaultTheme extends ThemeType {}
 }
