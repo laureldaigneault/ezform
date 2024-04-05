d8855f0c845fd97960ed5ec69173801d2eeb7ef5
LaureDaigneault <laurel2195@gmail.com>
2024-04-05 10:07:01 -0500

    fix: build pipeline and local dev setup

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ const packageJson = require('./package.json');
 
 export default [
   {
-    input: 'src/index-build.ts',
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
